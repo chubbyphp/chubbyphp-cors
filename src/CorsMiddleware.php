31e3ebcf377bff12b659c341016bc5c7f6224cc6
Dominik Zogg <dominik.zogg@gmail.com>
2019-07-14 12:47:17 +0200

    middleware only

--- src/CorsMiddleware.php
+++ src/CorsMiddleware.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Chubbyphp\Cors;
 
+use Chubbyphp\Cors\Negotiation\HeadersNegotiatorInterface;
+use Chubbyphp\Cors\Negotiation\MethodNegotiatorInterface;
 use Chubbyphp\Cors\Negotiation\Origin\OriginNegotiatorInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -13,14 +16,24 @@ use Psr\Http\Server\RequestHandlerInterface;
 final class CorsMiddleware implements MiddlewareInterface
 {
     /**
+     * @var ResponseFactoryInterface
+     */
+    private $responseFactory;
+
+    /**
      * @var OriginNegotiatorInterface
      */
     private $originNegotiator;
 
     /**
-     * @var bool
+     * @var MethodNegotiatorInterface
      */
-    private $allowCredentials;
+    private $methodNegotiator;
+
+    /**
+     * @var HeadersNegotiatorInterface
+     */
+    private $headersNegotiator;
 
     /**
      * @var array
@@ -28,18 +41,40 @@ final class CorsMiddleware implements MiddlewareInterface
     private $exposeHeaders;
 
     /**
-     * @param OriginNegotiatorInterface $originNegotiator
-     * @param bool                      $allowCredentials
-     * @param array                     $exposeHeaders
+     * @var bool
+     */
+    private $allowCredentials;
+
+    /**
+     * @var int
+     */
+    private $maxAge;
+
+    /**
+     * @param ResponseFactoryInterface   $responseFactory
+     * @param OriginNegotiatorInterface  $originNegotiator
+     * @param MethodNegotiatorInterface  $methodNegotiator
+     * @param HeadersNegotiatorInterface $headersNegotiator
+     * @param string[]                   $exposeHeaders
+     * @param bool                       $allowCredentials
+     * @param int                        $maxAge
      */
     public function __construct(
+        ResponseFactoryInterface $responseFactory,
         OriginNegotiatorInterface $originNegotiator,
+        MethodNegotiatorInterface $methodNegotiator,
+        HeadersNegotiatorInterface $headersNegotiator,
+        array $exposeHeaders = [],
         bool $allowCredentials = false,
-        array $exposeHeaders = []
+        int $maxAge = 600
     ) {
+        $this->responseFactory = $responseFactory;
         $this->originNegotiator = $originNegotiator;
-        $this->allowCredentials = $allowCredentials;
+        $this->methodNegotiator = $methodNegotiator;
+        $this->headersNegotiator = $headersNegotiator;
         $this->exposeHeaders = $exposeHeaders;
+        $this->allowCredentials = $allowCredentials;
+        $this->maxAge = $maxAge;
     }
 
     /**
@@ -52,21 +87,149 @@ final class CorsMiddleware implements MiddlewareInterface
     {
         $allowOrigin = $this->originNegotiator->negotiate($request);
 
-        $request = $request->withAttribute('allowOrigin', $allowOrigin);
-
-        $response = $handler->handle($request);
-
-        if (null !== $allowOrigin) {
-            $response = $response
-                ->withHeader('Access-Control-Allow-Origin', $allowOrigin)
-                ->withHeader('Access-Control-Allow-Credentials', $this->allowCredentials ? 'true' : 'false')
-            ;
-
-            if ([] !== $this->exposeHeaders) {
-                $response = $response->withHeader('Access-Control-Expose-Headers', implode(', ', $this->exposeHeaders));
-            }
+        if ($this->isPreflight($request)) {
+            return $this->handlePreflight($request, $allowOrigin);
         }
 
+        return $this->handle($request, $handler, $allowOrigin);
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     *
+     * @return bool
+     */
+    private function isPreflight(ServerRequestInterface $request): bool
+    {
+        return 'OPTIONS' === strtoupper($request->getMethod());
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param string|null            $allowOrigin
+     *
+     * @return ResponseInterface
+     */
+    private function handlePreflight(ServerRequestInterface $request, ?string $allowOrigin): ResponseInterface
+    {
+        $response = $this->responseFactory->createResponse(204);
+
+        if (null === $allowOrigin) {
+            return $response;
+        }
+
+        $response = $this->addAllowOrigin($response, $allowOrigin);
+        $response = $this->addAllowCredentials($response);
+        $response = $this->addExposeHeaders($response);
+
+        if (!$this->methodNegotiator->negotiate($request)) {
+            return $response;
+        }
+
+        $response = $this->addAllowMethod($response);
+
+        if (!$this->headersNegotiator->negotiate($request)) {
+            return $response;
+        }
+
+        $response = $this->addAllowHeaders($response);
+
+        return $this->addMaxAge($response);
+    }
+
+    /**
+     * @param ServerRequestInterface  $request
+     * @param RequestHandlerInterface $handler
+     * @param string|null             $allowOrigin
+     *
+     * @return ResponseInterface
+     */
+    private function handle(
+        ServerRequestInterface $request,
+        RequestHandlerInterface $handler,
+        ?string $allowOrigin
+    ): ResponseInterface {
+        $response = $handler->handle($request);
+
+        if (null === $allowOrigin) {
+            return $response;
+        }
+
+        $response = $this->addAllowOrigin($response, $allowOrigin);
+        $response = $this->addAllowCredentials($response);
+        $response = $this->addExposeHeaders($response);
+
         return $response;
+    }
+
+    /**
+     * @param ResponseInterface $response
+     * @param string            $allowOrigin
+     *
+     * @return ResponseInterface
+     */
+    private function addAllowOrigin(ResponseInterface $response, string $allowOrigin): ResponseInterface
+    {
+        return $response->withHeader('Access-Control-Allow-Origin', $allowOrigin);
+    }
+
+    /**
+     * @param ResponseInterface $response
+     *
+     * @return ResponseInterface
+     */
+    private function addAllowCredentials(ResponseInterface $response): ResponseInterface
+    {
+        return $response->withHeader('Access-Control-Allow-Credentials', $this->allowCredentials ? 'true' : 'false');
+    }
+
+    /**
+     * @param ResponseInterface $response
+     *
+     * @return ResponseInterface
+     */
+    private function addExposeHeaders(ResponseInterface $response): ResponseInterface
+    {
+        if ([] === $this->exposeHeaders) {
+            return $response;
+        }
+
+        return $response->withHeader('Access-Control-Expose-Headers', implode(', ', $this->exposeHeaders));
+    }
+
+    /**
+     * @param ResponseInterface $response
+     *
+     * @return ResponseInterface
+     */
+    private function addAllowMethod(ResponseInterface $response): ResponseInterface
+    {
+        return $response->withHeader(
+            'Access-Control-Allow-Methods',
+            implode(', ', $this->methodNegotiator->getAllowedMethods())
+        );
+    }
+
+    /**
+     * @param ResponseInterface $response
+     *
+     * @return ResponseInterface
+     */
+    private function addAllowHeaders(ResponseInterface $response): ResponseInterface
+    {
+        return $response->withHeader(
+            'Access-Control-Allow-Headers',
+            implode(', ', $this->headersNegotiator->getAllowedHeaders())
+        );
+    }
+
+    /**
+     * @param ResponseInterface $response
+     *
+     * @return ResponseInterface
+     */
+    private function addMaxAge(ResponseInterface $response): ResponseInterface
+    {
+        return $response->withHeader('Access-Control-Max-Age', (string) $this->maxAge);
     }
 }
